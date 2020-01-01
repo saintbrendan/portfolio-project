4d0defb6483ba9ca0f0c0d4d7f47a41882917852
Brendan Cunnie <saintbrendan@gmail.com>
2020-01-01 00:04:01 -0500

    Blog app up and running

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=255, default='')
+    pub_date = models.DateField(default=datetime.date.today)
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
