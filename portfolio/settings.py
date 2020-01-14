aedf847b791439ae10d53748329086b5ea60e216
Brendan Cunnie <saintbrendan@gmail.com>
2020-01-13 22:12:41 -0500

    Update password and hit Jobs table

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD': 'p1brendan',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432',
     }
