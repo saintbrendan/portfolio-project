0801ac37d687469cc4c30d815106a6a1b094a117
Brendan Cunnie <saintbrendan@gmail.com>
2020-02-01 15:00:49 -0500

    Add static files (resume pdf and personal picture jpg)

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -123,8 +123,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'portfolio/static/')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # the actual path on your drive
+STATIC_URL = '/static/'  # the virtual path
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 MEDIA_URL = '/media/'
